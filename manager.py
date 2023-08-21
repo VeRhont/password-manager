3d59153a67c4f086d5883c4b39e60bd45f32ce4d
Semyon Ivanov <semeni07@mail.ru>
2023-08-21 18:28:36 +0300

    Initial Commit

---
+++ manager.py
@@ -0,0 +1,54 @@
+from password import generate_password
+import json
+
+
+def create_account(website, login):
+    password = generate_password()
+
+    user_data = {
+        "website": website,
+        "login": login,
+        "password": password
+    }
+
+    with open("data.json", "r", encoding="utf-8") as file:
+        file_data = json.load(file)
+
+    with open("data.json", "w") as file:
+        file_data.append(user_data)
+        json.dump(file_data, file, indent=4)
+
+    print()
+    print(f"Your generated password: {password}")
+
+
+def get_account_password(website, login):
+    with open("data.json", "r", encoding="utf-8") as file:
+        file_data = json.load(file)
+
+        for item in file_data:
+            if (item.get("website") == website) and (item.get("login") == login):
+                print(item)
+
+
+def main():
+    temp = int(input("0 - create account\t1 - get password\n--> "))
+
+    if temp == 0:
+        website = input("website: ")
+        login = input("login: ")
+
+        create_account(website, login)
+
+    elif temp == 1:
+        website = input("website: ")
+        login = input("login: ")
+
+        get_account_password(website, login)
+
+    else:
+        print("Invalid command")
+
+
+if __name__ == "__main__":
+    main()
--- password.py
+++ password.py
@@ -2,16 +2,16 @@ from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from random import choice
 
 
-def generate_password(length=8, nums=True, lower=False, upper=False, special=False):
-    available = ''
+def generate_password(length=20, nums=True, lower=True, upper=True, special=True):
+    available = ""
     if nums: available += digits
     if lower: available += ascii_lowercase
     if upper: available += ascii_uppercase
     if special: available += punctuation
 
-    password = ''.join(choice(available) for _ in range(length))
+    password = "".join(choice(available) for _ in range(length))
     return password
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(generate_password())
